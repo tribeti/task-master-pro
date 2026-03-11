33101c86c632568f27a731508cd8cad71af4a0f8
tribeti <117581928+tribeti@users.noreply.github.com>
2026-03-11 11:25:55 +0700

    fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  db: {
-    schema: "main",
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey);
